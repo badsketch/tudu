b72f65ef34572aa41e8036e20f6ac7dfff10f130
Danielwang405 <danielwang405@gmail.com>
2019-08-19 00:49:49 -0400

    created header for now

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div id="header">
+        <h2 >Tudu</h2>
+        <h5>a task organizer (also a kanban board if you squint hard enough)</h5>
+        
+        
+        </div>
+    )
+}
