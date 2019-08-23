158135a1f7a01762f949681a562b76ca172af245
Danielwang405 <danielwang405@gmail.com>
2019-08-23 15:40:02 -0400

    made user filter row of bubbles

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -11,27 +11,30 @@ import {
 function Filter(props) {
 
     const [filterUserId, setUserFilter] = useState(1)
-    const handleSelectChange = e => {
-        console.log(e.target.value);
-        const userId = parseInt(e.target.value);
+
+    // const handleSelectChange = e => {
+    //     console.log(e.target.value);
+    //     const userId = parseInt(e.target.value);
+    //     setUserFilter(userId);
+    //     props.dispatch(FilterByUser(userId));
+    // }
+
+    const handleSelectChange = (userId) => {
+        // const userId = parseInt(e.target.value);
         setUserFilter(userId);
         props.dispatch(FilterByUser(userId));
     }
 
-    const optionStyles = {
-        height: 30,
-        width: 30,
-        borderRadius: '50%',
-    }
     return (
         <div id="visibility-filter">
+            Status: 
             <input type="radio" name="general" value="all" 
             onClick={() => props.dispatch(ResetFilter())}/>All
             <input type="radio" name="general" value="complete" 
                 onClick={() => props.dispatch(FilterByComplete())}/>Complete
             <input type="radio" name="general" value="incomplete" 
                 onClick={() => props.dispatch(FilterByIncomplete())}/>Incomplete
-            <select onChange={handleSelectChange}>
+            {/* <select onChange={handleSelectChange}>
                 <option key={-1} value={-1}>-</option>
                 {
                     props.userList.map(([id, { username, color }]) => {
@@ -41,16 +44,29 @@ function Filter(props) {
                                 value={id}
                             >
                                 {username}
-                                {/* <span 
-                                    style={{...optionStyles, background: color }}>
-                                    
-                                </span> */}
                             </option>
 
                         )
                     })
                 }
-            </select>
+            </select> */}
+            <div className="user-filter">
+                User: 
+            {
+                props.userList.map(([id, { username, color }]) => {
+                    return ( 
+                    <div 
+                        key={id}
+                        className="user-dot"
+                        style={{ 
+                            background: color,
+                            border: filterUserId == id ? '1px solid black' : ''
+                         }}
+                        onClick={() => handleSelectChange(parseInt(id))}></div>
+                    )
+                })
+            }
+            </div>
         </div>
     )
 }
